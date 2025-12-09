c9e16b016eae504bf31750a9718ad36245e734bc
yutatakabatake <yutatakabatake@gmail.com>
2025-12-09 18:02:46 +0900

    Modify INFIX-lang syntax and reduction rule

--- samples/INFIX-lang.rkt
+++ samples/INFIX-lang.rkt
@@ -9,11 +9,10 @@
      ;  (fun x -> e)
      ;  (if e e e)
      ;  (e e)
-     (e A e))
-  (A +
-     -
-     *
-     /))
+     (e + e)
+     (e - e)
+     (e * e)
+     (e / e)))
 
 (define INFIX?
   (lambda (x) (redex-match? INFIX e x)))
@@ -24,26 +23,46 @@
 (define ex2
   (term (1 + (2 + (3 + 4)))))
 
+(define ex3
+  (term(l-brace (1 + 2) r-brace)))
+
 (module+ test
   (test-equal (INFIX? ex1)
               #t)
   (test-equal (INFIX? ex2)
+              #t)
+  (test-equal (INFIX? ex3)
               #t))
 
 (define-extended-language Ev INFIX
   (E hole
      ;  (E e)
      ;  (v E)
-     (E A e)
-     (v A E)
+     (E + e)
+     (v + E)
+     (E - e)
+     (v - E)
+     (E * e)
+     (v * E)
+     (E / e)
+     (v / E)
      (l-brace E r-brace)))
 
 (define red
   (reduction-relation
    Ev #:domain e
-   (--> (in-hole E (v_1 A v_2))
-        (in-hole E (calc A v_1 v_2))
-        "calc")
+   (--> (in-hole E (v_1 + v_2))
+        (in-hole E ,(apply + (term (v_1 v_2))))
+        "+")
+   (--> (in-hole E (v_1 - v_2))
+        (in-hole E ,(apply - (term (v_1 v_2))))
+        "-")
+   (--> (in-hole E (v_1 * v_2))
+        (in-hole E ,(apply * (term (v_1 v_2))))
+        "*")
+   (--> (in-hole E (v_1 / v_2))
+        (in-hole E ,(apply / (term (v_1 v_2))))
+        "/")
    (--> (in-hole E (l-brace e r-brace))
         (in-hole E (l-brace e_new r-brace))
         (where e_new (apply-reduction-relation red e))
@@ -51,17 +70,6 @@
    (--> (in-hole E (l-brace v r-brace))
         (in-hole E v)
         "v-brace")))
-
-(define-metafunction Ev
-  calc : A v v -> v
-  [(calc + v_1 v_2)
-   ,(apply + (term (v_1 v_2)))]
-  [(calc - v_1 v_2)
-   ,(apply - (term (v_1 v_2)))]
-  [(calc * v_1 v_2)
-   ,(apply * (term (v_1 v_2)))]
-  [(calc / v_1 v_2)
-   ,(apply / (term (v_1 v_2)))])
 
 (define run
   (lambda (p)
